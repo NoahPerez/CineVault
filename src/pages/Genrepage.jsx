e538a7a4063b5aa83a6bc8b7d56bbd33756857a5
Almas Khan <khanalmas753@gmail.com>
2026-06-06 12:12:41 +0200

    Added Genre

--- src/pages/Genrepage.jsx
+++ src/pages/Genrepage.jsx
@@ -1,37 +1,4 @@
-// import { useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import MovieCard from "../components/MovieCard";
-// import { useMovies } from "../context/Movie.context";
 
-// function GenrePage() {
-//   const { id } = useParams();
-//   const { movies, loading, error, getMoviesByGenre } = useMovies();
-
-//   useEffect(() => {
-//     getMoviesByGenre(id);
-//   }, [id, getMoviesByGenre]);
-
-//   if (loading) return <div>Loading...</div>;
-//   if (error) return <div>Error: {error}</div>;
-
-//   return (
-//     <section style={{ padding: "24px" }}>
-//       <h1>Genre Movies</h1>
-
-//       <div style={{
-//         display: "grid",
-//         gridTemplateColumns: "repeat(auto-fit, minmax(220px, 1fr))",
-//         gap: "24px",
-//       }}>
-//         {movies.map((movie) => (
-//           <MovieCard key={movie.id} movie={movie} />
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default GenrePage;
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useMovies } from "../context/Movie.context";
